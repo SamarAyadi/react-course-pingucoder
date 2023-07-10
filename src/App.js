edaaccf9720335faf0e45b5afdf2f44453537b55
Samar AYADI <ayadisamardev@gmail.com>
2023-07-10 22:06:09 +0100

    List Rendering Challenge

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import Post from "./Post";
 import SideMenu from "./SideMenu";
 
 function App() {
+  const posts = [{
+    id: 1,
+    postName: "PinguCoder",
+    postBody: "platform e learning"
+  },
+  {
+    id: 2,
+    postName: "Learn React",
+    postBody: "With PinguCoder"
+    },
+    {
+      id: 3,
+      postName: "Learn Angular",
+      postBody: "With PinguCode"
+    }
+  ];
+  const postList = posts.map((post) => {
+    return <Post key={post.id} postName={post.postName} postBody={post.postBody} />
+  })
   return (
     <div className="App">
       <Header />
@@ -15,15 +34,7 @@ function App() {
         >
           {/* POSTS CONTAINER */}
           <div style={{ width: "70%" }}>
-            <Post postName="PinguCoder" postBody="platform e learning" />
-            <Post postName="Learn React" postBody="With PinguCoder" />
-            <Post postName="Learn Angular" postBody="With PinguCoder" />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+           {postList}
           </div>
 
           <div style={{ width: "30%", marginTop: "25px" }}>
--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,26 +1,50 @@
 import TagButton from "./TagButton";
 
 function SideMenu() {
-  return (
-    <div style={{ margin: "25px", border: "solid #1d9bf0 5px" }}>
-      <TagButton title="Programming">
+  const categories = [
+    {
+      uniqueId: 1,
+      title: "Programming",
+      c: (
         <div>
           <span>🐧🐧🐧</span>
         </div>
-      </TagButton>
-      <TagButton title="Data Science">
+      ),
+    },
+    {
+      uniqueId: 2,
+      title: "Data Science",
+      c: (
         <div>
-          <img style={{ width: "100px"}} src="https://images.unsplash.com/photo-1688588428113-d24195a109bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1033&q=80" alt=""/>
+          <img
+            style={{ width: "100px" }}
+            src="https://images.unsplash.com/photo-1688588428113-d24195a109bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1033&q=80"
+            alt=""
+          />
         </div>
+      ),
+    },
+    {
+      uniqueId: 3,
+      title: "Technology",
+      c: (
+        <>
+          <h1>Click me</h1>
+          <span>😎😎😎</span>
+        </>
+      ),
+    },
+  ];
+  const categoriesList = categories.map((cat) => {
+    return (
+      <TagButton key={cat.uniqueId} title={cat.title}>
+        {cat.c}
       </TagButton>
-      <TagButton title="Technology" >
-        <h1>Click me</h1>
-        <span>😎😎😎</span>
-      </TagButton>
-      <TagButton title="Self Improvement" />
-      {/* <TagButton />
-      <TagButton />
-      <TagButton /> */}
+    );
+  });
+  return (
+    <div style={{ margin: "25px", border: "solid #1d9bf0 5px" }}>
+      {categoriesList}
     </div>
   );
 }
