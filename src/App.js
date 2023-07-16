df0447c4d3caab3336534655de85c4cbc5c4735e
Samar AYADI <ayadisamardev@gmail.com>
2023-07-16 03:21:18 +0100

    Handling Inputs with Forms

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import "./App.css";
 import Header from "./Header";
-import Post from "./Post";
-import SideMenu from "./SideMenu";
+import MyButton from "./MyButton";
+
 
 function App() {
-  const posts = [{
-    id: 1,
-    postName: "PinguCoder",
-    postBody: "platform e learning"
-  },
-  {
-    id: 2,
-    postName: "Learn React",
-    postBody: "With PinguCoder"
-    },
-    {
-      id: 3,
-      postName: "Learn Angular",
-      postBody: "With PinguCode"
-    }
-  ];
-  const postList = posts.map((post) => {
-    return <Post key={post.id} postName={post.postName} postBody={post.postBody} />
-  })
+  
   return (
     <div className="App">
       <Header />
-
-      {/* POSTS CONTAINER */}
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div
-          style={{ width: "60%", display: "flex", justifyContent: "center" }}
-        >
-          {/* POSTS CONTAINER */}
-          <div style={{ width: "70%" }}>
-           {postList}
-          </div>
-
-          <div style={{ width: "30%", marginTop: "25px" }}>
-            <SideMenu />
-          </div>
-        </div>
-      </div>
+       <MyButton />
     </div>
   );
 }
