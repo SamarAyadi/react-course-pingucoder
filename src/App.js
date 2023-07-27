39a4d623a73bb4d626300ad061d66e7dff0b1410
Samar AYADI <ayadisamardev@gmail.com>
2023-07-27 21:29:49 +0100

    Object's State Challenge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "./Header";
 import MyForms from "./MyForms";
+import Scoreboard from "./Scoreboard"
 
 
 function App() {
   return (
     <div className="App" style={{paddingTop: "30px"}}>
-      <Header />
-      <MyForms />
+      {/* <Header />
+      <MyForms /> */}
+      <Scoreboard />
     </div>
   );
 }
